3647ae6b2661acd1b27fe8d831575ff09a285a36
Marko Urbanc <urbancmarko1@gmail.com>
2023-11-08 01:30:52 +0100

    feat: :sparkles: add two stationary cases of HydrogenFusion object

--- HydrogenFusion/Code/hydrogenfusion.py
+++ HydrogenFusion/Code/hydrogenfusion.py
@@ -84,4 +84,116 @@ class HydrogenFusion:
         p, q, r, s, t = self.rates
 
         return t*v*y + p*v - q*z**2
+    
+class HydrogenFusionStationaryH(HydrogenFusion):
+    def __init__(self, init_conc: Iterable[float], t_eval: Iterable[float], rates: Iterable[float]) -> None:
+        """
+        A stationary solution for the rate equations where the concentration of
+        hydrogen is constant. Thus the rate equations for hydrogen are set to zero.
+        That way we can write the term:
+
+        s*x*y = r*u*z - t*v*y
+        """
+        super().__init__(init_conc, t_eval, rates)
+
+    def _du(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return -t*v*y 
+    
+    def _dv(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return q*z**2 - p*v - t*v*y
+    
+    def _dx(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return 2*t*v*y
+    
+    def _dy(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return 0
+    
+    def _dz(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return t*v*y + p*v - q*z**2
+    
+class HydrogenFusionStationaryBr(HydrogenFusion):
+    def __init__(self, init_conc: Iterable[float], t_eval: Iterable[float], rates: Iterable[float]) -> None:
+        """
+        A stationary solution for the rate equations where the concentration of
+        bromine is constant. Thus the rate equations for bromine are set to zero.
+        That way we can write the term:
+
+        t*v*y = q*z**2 - p*v
+        """
+        super().__init__(init_conc, t_eval, rates)
+
+    def _du(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return s*x*y - r*u*z
+    
+    def _dv(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return 0
+    
+    def _dx(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+        
+        return q*z**2 - p*v - s*x*y + r*u*z
+    
+    def _dy(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return r*u*z - s*x*y - q*z**2 + p*v
+    
+    def _dz(self, t_eval, state):
+        # Unpack the state vector
+        u, v, x, y, z = state
+        
+        # Get rate coefficients
+        p, q, r, s, t = self.rates
+
+        return 0
 
