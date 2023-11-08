6404f27826e6ceeb61679abdb18ae4347700ca66
Marko Urbanc <urbancmarko1@gmail.com>
2023-11-08 23:41:14 +0100

    feat: :art: add rate plot

--- HydrogenFusion/Code/quintic-pars.py
+++ HydrogenFusion/Code/quintic-pars.py
@@ -16,25 +16,74 @@ init_conc = np.array([1, 1, 0, 0, 0])
 rates = np.array([1, 1, 1, 1, 1])
 t_eval = np.linspace(0, 10, 1000)
 
-solutions = []
+# This solution is terrible, but I'm in a hurry.
+solutions_a = []
+solutions_b = []
+solutions_c = []
+solutions_d = []
+solutions_e = []
+solutions_f = []
 H2_range = np.linspace(0.1, 10, 100)
 Br2_range = np.linspace(0.1, 10, 100)
 
-for i, H2 in enumerate(H2_range):
-    print(f"Progress: {i}/{len(H2_range)}")
-    column = []
-    for Br2 in Br2_range:
-        rates = np.array([H2, Br2, 1, 1, 1])
-        hf = HydrogenFusion(init_conc, t_eval, rates)
-        sol = hf.solve()
-        sol.y = sol.y / np.sum(init_conc)
-        column.append(fit_func(sol.t, sol.y[2], quintic)[1][2])
+if True:
+    for i, H2 in enumerate(H2_range):
+        print(f"Progress: {i}/{len(H2_range)}")
+        # Ditto.
+        column_a = []
+        column_b = []
+        column_c = []
+        column_d = []
+        column_e = []
+        column_f = []
+        for Br2 in Br2_range:
+            rates = np.array([H2, Br2, 1, 1, 1])
+            hf = HydrogenFusion(init_conc, t_eval, rates)
+            sol = hf.solve()
+            sol.y = sol.y / np.sum(init_conc)
+            column_a.append(fit_func(sol.t, sol.y[2], quintic)[1][0])
+            column_b.append(fit_func(sol.t, sol.y[2], quintic)[1][1])
+            column_c.append(fit_func(sol.t, sol.y[2], quintic)[1][2])
+            column_d.append(fit_func(sol.t, sol.y[2], quintic)[1][3])
+            column_e.append(fit_func(sol.t, sol.y[2], quintic)[1][4])
+            column_f.append(fit_func(sol.t, sol.y[2], quintic)[1][5])
+    
+        solutions_a.append(column_a)
+        solutions_b.append(column_b)
+        solutions_c.append(column_c)
+        solutions_d.append(column_d)
+        solutions_e.append(column_e)
+        solutions_f.append(column_f)
+    
+    solutions_a = np.array(solutions_a)
+    df_a = pd.DataFrame(solutions_a, index=H2_range, columns=Br2_range)
+    df_a.to_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='a', mode='w', complevel=9)
+    df_b = pd.DataFrame(solutions_b, index=H2_range, columns=Br2_range)
+    df_b.to_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='b', mode='a', complevel=9)
+    df_c = pd.DataFrame(solutions_c, index=H2_range, columns=Br2_range)
+    df_c.to_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='c', mode='a', complevel=9)
+    df_d = pd.DataFrame(solutions_d, index=H2_range, columns=Br2_range)
+    df_d.to_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='d', mode='a', complevel=9)
+    df_e = pd.DataFrame(solutions_e, index=H2_range, columns=Br2_range)
+    df_e.to_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='e', mode='a', complevel=9)
+    df_f = pd.DataFrame(solutions_f, index=H2_range, columns=Br2_range)
+    df_f.to_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='f', mode='a', complevel=9)
+    quit()
 
-    solutions.append(column)
+# Load the data.
+df = pd.read_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='a')
+solutions_a = df.values
+df = pd.read_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='b')
+solutions_b = df.values
+df = pd.read_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='c')
+solutions_c = df.values
+df = pd.read_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='d')
+solutions_d = df.values
+df = pd.read_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='e')
+solutions_e = df.values
+df = pd.read_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='f')
+solutions_f = df.values
 
-solutions = np.array(solutions)
-df = pd.DataFrame(solutions, index=H2_range, columns=Br2_range)
-df.to_hdf('./HydrogenFusion/Results/quintic-pars.h5', key='Fit', mode='w', complevel=9)
 
 # Plot
 fig, ax = plt.subplots(2, 3, figsize=(10, 10))
@@ -45,12 +94,13 @@ cm_b = pl.colorbrewer.sequential.BuGn_7.mpl_colormap
 cm_c = pl.colorbrewer.sequential.OrRd_7.mpl_colormap
 cm_d = pl.colorbrewer.sequential.PuBu_7.mpl_colormap
 cm_e = pl.colorbrewer.sequential.PuRd_7.mpl_colormap
+cm_f = pl.colorbrewer.sequential.RdPu_7.mpl_colormap
 
 # DEBUG plot first subplot
-ax[0].contourf(H2_range, Br2_range, solutions[1][0], levels=100, cmap=cm_a)
+ax[0, 0].contourf(H2_range, Br2_range, solutions_a, levels=100, cmap=cm_a)
 
 
-ax[0].set_xlabel(r'Initial concentration of $H_2$')
-ax[0].set_ylabel(r'Initial concentration of $Br_2$')
+ax[0, 0].set_xlabel(r'Initial concentration of $H_2$')
+ax[0, 0].set_ylabel(r'Initial concentration of $Br_2$')
 
 plt.show()
--- HydrogenFusion/Code/rate-of-synthesis.py
+++ HydrogenFusion/Code/rate-of-synthesis.py
@@ -19,19 +19,23 @@ t_eval = np.linspace(0, 10, 1000)
 init_conc1 = np.array([100, 1, 0, 0, 0])
 init_conc2 = np.array([1, 1, 0, 0, 0])
 init_conc3 = np.array([1, 100, 0, 0, 0])
+init_conc4 = np.array([1, 1, 1e3, 0, 0])
 
 hf1 = HydrogenFusion(init_conc1, t_eval, rates)
 hf2 = HydrogenFusion(init_conc2, t_eval, rates)
 hf3 = HydrogenFusion(init_conc3, t_eval, rates)
+hf4 = HydrogenFusion(init_conc4, t_eval, rates)
 
 sol1 = hf1.solve()
 sol2 = hf2.solve()
 sol3 = hf3.solve()
+sol4 = hf4.solve()
 
 # Normalize the solutions to the sum of the initial concentrations
 sol1.y = sol1.y / np.sum(init_conc1)
 sol2.y = sol2.y / np.sum(init_conc2)
 sol3.y = sol3.y / np.sum(init_conc3)
+sol4.y = sol4.y / np.sum(init_conc4)
 
 # Plot
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
@@ -42,12 +46,15 @@ custom_colors = ["#9750a1", "#00c9bf", "#48817c"]
 ax[0].plot(sol1.t, sol1.y[2], color=custom_colors[0], label=r'$\frac{\mathrm{H}_2}{\mathrm{Br}_2} = 100$')
 ax[0].plot(sol2.t, sol2.y[2], color=custom_colors[1], label=r'$\frac{\mathrm{H}_2}{\mathrm{Br}_2} = 1$')
 ax[0].plot(sol3.t, sol3.y[2], color=custom_colors[2], label=r'$\frac{\mathrm{H}_2}{\mathrm{Br}_2} = 0.01$')
+ax[0].plot(sol4.t, sol4.y[2], color="#97e364", label=r'$\frac{\mathrm{HBr}}{\mathrm{Br}_2} = 10^3$')
 
 ax[0].set_xlabel('Time [arb. units]')
 ax[0].set_ylabel('Relative Concentration')
 
-ax[0].legend(loc='upper left', ncol=1, frameon=False)
+ax[0].legend(loc='center left', ncol=1, frameon=False)
 ax[0].grid(color="#424656", alpha=0.1)
+
+ax[0].set_title("Rate of Synthesis of $HBr$ in the\nPresence of Different Initial Concentrations")
 
 # --- Continuous plot ---
 # Define the initial concentrations.
@@ -55,37 +62,44 @@ solutions = []
 H2_range = np.linspace(0.001, 100, 100)
 Br2_range = np.linspace(0.001, 100, 100)
 
-for i, H2 in enumerate(H2_range):
-    print(f"Progress: {i}/{len(H2_range)}")
-    column = []
-    for Br2 in Br2_range:
-        init_conc = np.array([H2, Br2, 0, 0, 0])
-        hf = HydrogenFusion(init_conc, t_eval, rates)
-        sol = hf.solve()
-        sol.y = sol.y / np.sum(init_conc)
-        column.append(fit_func(sol.t, sol.y[2], quintic)[1][2])
+if False:
+    for i, H2 in enumerate(H2_range):
+        print(f"Progress: {i}/{len(H2_range)}")
+        column = []
+        for Br2 in Br2_range:
+            init_conc = np.array([H2, Br2, 0, 0, 0])
+            hf = HydrogenFusion(init_conc, t_eval, rates)
+            sol = hf.solve()
+            sol.y = sol.y / np.sum(init_conc)
+            column.append(fit_func(sol.t, sol.y[2], quintic)[1][-1])
+    
+        solutions.append(column)
+    
+    solutions = np.array(solutions)
+    df = pd.DataFrame(solutions, index=H2_range, columns=Br2_range)
+    df.to_hdf('./HydrogenFusion/Results/synthesis-leading.h5', key='Synthesis', mode='w', complevel=9)
+    quit()
 
-    solutions.append(column)
-
-solutions = np.array(solutions)
-df = pd.DataFrame(solutions, index=H2_range, columns=Br2_range)
-df.to_hdf('./HydrogenFusion/Results/synthesis.h5', key='Synthesis', mode='w', complevel=9)
-
-# Plot
+# Load the data 
+df = pd.read_hdf('./HydrogenFusion/Results/synthesis-leading.h5', key='Synthesis')
+solutions = df.values
 # Define the colors.
-norm = mpl.colors.Normalize(vmin=0, vmax=1)
+norm = mpl.colors.Normalize(vmin=solutions.min(), vmax=solutions.max())
 cm = pl.colorbrewer.sequential.BuPu_7.mpl_colormap
 
 ax[1].contourf(H2_range, Br2_range, solutions, levels=100, cmap=cm, norm=norm)
-ax[1].contour(H2_range, Br2_range, solutions, levels=20)
+ax[1].contour(H2_range, Br2_range, solutions, levels=20, linewidths=0.1, colors='k')
 
 ax[1].set_xlabel(r'Initial concentration of $H_2$')
 ax[1].set_ylabel(r'Initial concentration of $Br_2$')
 
+ax[1].set_title("Size of the Leading Coefficient in the\nQuintic Expansion for the Rate of Synthesis of $HBr$")
+
 sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
 cbar = plt.colorbar(sm, ax=ax[1])
-cbar.set_label('Size of the leading coef. in the quintic expansion for the rate of synthesis of $HBr$')
+cbar.set_label('Size of the leading coefficient\nin the quintic expansion')
 
+plt.subplots_adjust(left=0.06, right=0.93, bottom=0.1,)
 plt.show()
 
 
